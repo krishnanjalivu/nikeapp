dff957c3fcd3d0d40131ada150a5d32eb84ff265
krishnanjalivu <vukrishnanjali@gmail.com>
2023-10-17 20:51:47 +0530

    first commit

---
+++ src/sections/PopularProducts.jsx
@@ -0,0 +1,12 @@
+
+
+const PopularProducts = () => {
+  return (
+    <div>
+      PopularProducts
+    </div>
+  )
+}
+
+export default PopularProducts
+
---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import Hero from "./Hero.jsx";
+import PopularProducts from "./PopularProducts.jsx"
+import CustomerReviews from "./CustomerReviews.jsx"
+import Footer from "./Footer.jsx"
+import Services from "./Services.jsx"
+import SpecialOffer from "./SpecialOffer.jsx"
+import Subscribe from "./Subscribe.jsx"
+import SuperQuality from "./SuperQuality.jsx"
+
+
+export {
+    Hero,
+    PopularProducts,
+    CustomerReviews,
+    Footer,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality
+
+}
