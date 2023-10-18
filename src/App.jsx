552f9fbdfb0ddeff80b1e70c3b57062f9a13b6b3
krishnanjalivu <vukrishnanjali@gmail.com>
2023-10-18 15:06:27 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   return(
   <main className='relative'>
   <Nav />
-  <section className='xl:padding-l wide:padding-r padding-b'>
+  <section className='xl:px-20 wide:padding-r padding-b'>
     <Hero />
  </section>
   <section className='padding'>
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,9 +1,17 @@
-
+import ServiceCard from "../components/ServiceCard.jsx"
+import {services} from "../constants"
 
 const Services = () => {
   return (
     <div>
-      Services
+      <section className="max-container flex justify-center flex-wrap gap-9 ">
+      {
+        services.map((service)=>(
+          <ServiceCard key={service.label} {...service} />
+        ))
+      }
+        
+      </section>
     </div>
   )
 }
